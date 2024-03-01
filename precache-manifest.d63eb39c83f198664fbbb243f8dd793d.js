d64d3d8e0301ded0d6fdc4a82f10464629c9444c
SeolHwa <103579961+seolhwa-y@users.noreply.github.com>
2024-03-01 21:29:55 +0900

    Add files via upload

---
+++ precache-manifest.d63eb39c83f198664fbbb243f8dd793d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f515e19e6b6c31710809",
+    "url": "/static/css/main.a7e24269.chunk.css"
+  },
+  {
+    "revision": "f515e19e6b6c31710809",
+    "url": "/static/js/main.b19a377d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6153a5edb01963bc8d0",
+    "url": "/static/js/2.6a1e3ea7.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bf31541d08cfba28497b35f3ce8f5f9",
+    "url": "/index.html"
+  }
+];
